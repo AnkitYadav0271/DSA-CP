526144eb77ef6a76e9b9989c3e6f5b571690e997
Ankit Yadav <ankitofficial0271@gmail.com>
2026-05-22 21:29:59 +0530

    Longest Increasing Subsequence LC 300 recursive

---
+++ DP/LIS/LisRec.java
@@ -0,0 +1,34 @@
+import java.util.Arrays;
+
+class LisRec {
+    int  dp [][];
+    public static Lis (int [] nums){
+        dp = new int [nums.length + 1][nums.length + 1];
+
+        for(int []row : dp){
+            Arrays.fill(row,-1);
+        }
+
+        return solve(nums,0,-1);
+    }
+
+    public int solve(int arr[] , int currIndex , int prevIndex){
+        if(currIndex >= arr.length){
+            return 0;
+        }
+
+        if(dp[currIndex][prevIndex + 1] != -1){
+            return dp[currIndex][prevIndex+1];
+        }
+
+        int take = 0;
+
+        if(prevIndex == -1 || arr[prevIndex] < arr[currIndex]){
+            take = 1 + solve(arr, currIndex+ 1, i);
+        }
+
+        int skip = solve(arr,currIndex + 1, prevIndex);
+
+        return dp[currIndex][prevIndex + 1] = Math.max(skip,take); 
+    }
+}
